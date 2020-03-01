34d9323a3645fd5ad9c5a70125acbfd8fead47f4
dlstonedl <ldeng@thoughtworks.com>
2020-03-01 09:32:24 +0800

    parking boy repository

---
+++ src/test/java/com/dlstone/ddd/practice/parklot/infrastructure/ParkingBoyRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.dlstone.ddd.practice.parklot.infrastructure;
+
+import com.dlstone.ddd.practice.parklot.domain.model.ParkingBoy;
+import com.dlstone.ddd.practice.parklot.domain.model.ParkingBoyId;
+import com.dlstone.ddd.practice.parklot.domain.model.ParkingBoyRepository;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ParkingBoyRepositoryTest {
+    private ParkingBoyRepository parkingBoyRepository;
+
+    @Before
+    public void setUp() {
+        parkingBoyRepository = new ParkingBoyRepositoryImpl();
+    }
+
+    @Test
+    public void should_return_parking_boy() {
+        ParkingBoy parkingBoy = parkingBoyRepository.getParkingBoy();
+        assertNotNull(parkingBoy);
+    }
+
+    @Test
+    public void should_save_parking_boy() {
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingBoyId("test"));
+        parkingBoyRepository.saveParkingBoy(parkingBoy);
+        ParkingBoy boy = parkingBoyRepository.getParkingBoy();
+        assertEquals(parkingBoy, boy);
+    }
+
+
+
+}
