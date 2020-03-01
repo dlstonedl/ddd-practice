dfedcc5b05a41428e5702e169c8042b0cca99ceb
dlstonedl <ldeng@thoughtworks.com>
2020-03-01 10:19:54 +0800

    client

---
+++ src/main/java/com/dlstone/ddd/practice/parklot/Client.java
@@ -0,0 +1,37 @@
+package com.dlstone.ddd.practice.parklot;
+
+import com.dlstone.ddd.practice.parklot.common.exception.ParkingLotException;
+import com.dlstone.ddd.practice.parklot.domain.model.*;
+import com.dlstone.ddd.practice.parklot.infrastructure.ParkingBoyRepositoryImpl;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Client {
+
+    private static ParkingBoyRepository parkingBoyRepository = new ParkingBoyRepositoryImpl();
+
+    public static void main(String[] args) throws ParkingLotException {
+        Car car = new Car(new CarId("粤B000TA"));
+
+        Ticket ticket = park(car);
+        log.info("park ticket is {}", ticket);
+
+        Car takeCar = take(ticket);
+        log.info("take car {}", takeCar);
+    }
+
+    private static Ticket park(Car car) {
+        ParkingBoy parkingBoy = parkingBoyRepository.getParkingBoy();
+        Ticket ticket = parkingBoy.park(car);
+        parkingBoyRepository.saveParkingBoy(parkingBoy);
+        return ticket;
+    }
+
+    private static Car take(Ticket ticket) throws ParkingLotException {
+        ParkingBoy parkingBoy = parkingBoyRepository.getParkingBoy();
+        ParkingLot parkingLot = parkingBoy.getParkingLot(ticket);
+        Car car = parkingLot.take(ticket);
+        parkingBoyRepository.saveParkingBoy(parkingBoy);
+        return car;
+    }
+}
