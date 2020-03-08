f0eb55ceba8e411c336101316f6b02347571a42e
dlstonedl <ldeng@thoughtworks.com>
2020-03-09 07:18:02 +0800

    remove no use code

--- src/main/java/com/dlstone/ddd/practice/parklot/domain/model/ParkingBoyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dlstone.ddd.practice.parklot.domain.model;
-
-public interface ParkingBoyRepository {
-
-    ParkingBoy getParkingBoy();
-
-    void saveParkingBoy(ParkingBoy parkingBoy);
-}
