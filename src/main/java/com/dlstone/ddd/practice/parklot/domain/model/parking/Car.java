ec784380719f85bfbc059d44abc7c021d395f7d6
dlstonedl <ldeng@thoughtworks.com>
2020-03-14 23:30:46 +0800

    remove no use code

--- src/main/java/com/dlstone/ddd/practice/parklot/domain/model/parking/Car.java
+++ src/main/java/com/dlstone/ddd/practice/parklot/domain/model/parking/Car.java
@@ -1,12 +1,8 @@
 package com.dlstone.ddd.practice.parklot.domain.model.parking;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 import lombok.Value;
 
 @Value
-@ToString
-@EqualsAndHashCode
 public class Car {
     private final CarId id;
 }
--- src/main/java/com/dlstone/ddd/practice/parklot/domain/model/parking/Ticket.java
+++ src/main/java/com/dlstone/ddd/practice/parklot/domain/model/parking/Ticket.java
@@ -1,10 +1,8 @@
 package com.dlstone.ddd.practice.parklot.domain.model.parking;
 
-import lombok.ToString;
 import lombok.Value;
 
 @Value
-@ToString
 public class Ticket {
     private final CarId carId;
     private final ParkingLotId parkingLotId;
