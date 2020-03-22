cb8c6ba86503642a288b8ac61c565baa1cd02541
dlstonedl <ldeng@thoughtworks.com>
2020-03-22 09:11:59 +0800

    refactor

--- src/main/java/com/dlstone/ddd/practice/parklot/parking/adapter/DbParkingLotFinderFactory.java
+++ src/main/java/com/dlstone/ddd/practice/parklot/parking/adapter/DbParkingLotFinderFactory.java
@@ -1,14 +1,14 @@
 package com.dlstone.ddd.practice.parklot.parking.adapter;
 
 import com.dlstone.ddd.practice.parklot.config.application.ParkingConfigApplicationService;
-import com.dlstone.ddd.practice.parklot.config.domain.ParkingBoy;
 import com.dlstone.ddd.practice.parklot.config.domain.ParkingBoyId;
-import com.dlstone.ddd.practice.parklot.config.domain.ParkingManager;
 import com.dlstone.ddd.practice.parklot.config.domain.ParkingStrategyName;
 import com.dlstone.ddd.practice.parklot.parking.domain.core.ParkingLot;
 import com.dlstone.ddd.practice.parklot.parking.domain.core.ParkingLotFinder;
 import com.dlstone.ddd.practice.parklot.parking.domain.core.ParkingLotFinderFactory;
 import com.dlstone.ddd.practice.parklot.parking.domain.core.ParkingLotRepository;
+import com.dlstone.ddd.practice.parklot.parking.domain.finder.ParkingBoy;
+import com.dlstone.ddd.practice.parklot.parking.domain.finder.ParkingManager;
 import com.dlstone.ddd.practice.parklot.parking.domain.finder.Strategy;
 import com.dlstone.ddd.practice.parklot.parking.domain.policy.MaxIdleStrategy;
 import com.dlstone.ddd.practice.parklot.parking.domain.policy.SortedStrategy;
@@ -25,24 +25,24 @@ public class DbParkingLotFinderFactory implements ParkingLotFinderFactory {
 
     @Override
     public ParkingLotFinder newParkingManager() {
-        ParkingManager parkingManager = parkingConfigApplicationService.getParkingManager();
+        com.dlstone.ddd.practice.parklot.config.domain.ParkingManager parkingManager = parkingConfigApplicationService.getParkingManager();
 
-        List<com.dlstone.ddd.practice.parklot.parking.domain.finder.ParkingBoy> parkingBoys = parkingManager.getParkingBoyIds()
+        List<ParkingBoy> parkingBoys = parkingManager.getParkingBoyIds()
             .stream()
             .map(this::newParkingBoy)
-            .map(parkingLotFinder -> (com.dlstone.ddd.practice.parklot.parking.domain.finder.ParkingBoy) parkingLotFinder)
+            .map(parkingLotFinder -> (ParkingBoy) parkingLotFinder)
             .collect(Collectors.toList());
 
-        return new com.dlstone.ddd.practice.parklot.parking.domain.finder.ParkingManager(parkingBoys);
+        return new ParkingManager(parkingBoys);
     }
 
     @Override
     public ParkingLotFinder newParkingBoy(ParkingBoyId parkingBoyId) {
-        ParkingBoy parkingBoy = parkingConfigApplicationService.getParkingBoy(parkingBoyId);
+        com.dlstone.ddd.practice.parklot.config.domain.ParkingBoy parkingBoy = parkingConfigApplicationService.getParkingBoy(parkingBoyId);
 
         List<ParkingLot> parkingLots = parkingLotRepository.getParkingLots(parkingBoy.getParkingLotIds());
         Strategy strategy = parkingBoy.getParkingStrategyName() == ParkingStrategyName.SORTED ? new SortedStrategy() : new MaxIdleStrategy();
 
-        return new com.dlstone.ddd.practice.parklot.parking.domain.finder.ParkingBoy(parkingLots, strategy);
+        return new ParkingBoy(parkingLots, strategy);
     }
 }
